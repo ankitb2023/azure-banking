f54cf3f37459dceec93ab141bc0abb155e1fe59b
Ankit Bhujeja <bhujejaankit@gmail.com>
2022-02-11 22:08:46 +0530

    First

---
+++ history.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/table.css">
+    <title>BANK OF SPAIN- Transaction History</title>
+     <style>
+       @import url('https://fonts.googleapis.com/css2?family=Luxurious+Roman&family=Oswald:wght@500&family=Permanent+Marker&display=swap');
+
+       .navigation{
+    background-color: #ff0707!important;
+    
+        }
+        .navigation .navbar-brand{
+            font-size: 1.8rem;
+            padding-left: 40px;
+            font-family: 'Permanent Marker', cursive;
+            letter-spacing: 0.8px;
+            word-spacing: 1px;
+            
+            color: aliceblue!important;    
+        }
+        .nav-link{
+            font-weight: bold;
+            font-size: 15px; 
+            color: antiquewhite!important; 
+        }
+        .navigation .navbar-brand:hover{
+          color:black!important;
+      }
+     </style>
+</head>
+
+<body>
+    
+     
+    <!-- navigation bar -->
+
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light  py-1 navigation ">
+            <a class="navbar-brand" href="#">BANK OF SPAIN</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item ml-4">
+                  <a class="nav-link" href="index.html">HOME</a>
+                </li>
+                <li class="nav-item ml-4">
+                  <a class="nav-link" href="contact.html">CONTACT</a>
+                </li>
+                <li class="nav-item ml-4">
+                  <a class="nav-link" href="about.html">ABOUT</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+     </header>
+
+ 
+     <br><br><br>
+
+     <div class="container">
+        <h2 class="text-center pt-4">Transaction History</h2>
+        
+       <br>
+       <div class="table-responsive-sm">
+    <table class="table table-hover table-striped table-condensed table-bordered">
+        <thead>
+            <tr>
+                <th class="text-center">Sr No.</th>
+                <th class="text-center">Sender</th>
+                <th class="text-center">Receiver</th>
+                <th class="text-center">Amount( In €)</th>
+                
+            </tr>
+        </thead>
+        <tbody>
+
+        <?php
+
+            include 'connectdb.php';
+
+            $sql ="select * from transaction";
+
+            $query =mysqli_query($conn, $sql);
+
+            while($rows = mysqli_fetch_assoc($query))
+            {
+        ?>
+
+            <tr>
+            <td class="py-2"><?php echo $rows['Sno']; ?></td>
+            <td class="py-2"><?php echo $rows['sender']; ?></td>
+            <td class="py-2"><?php echo $rows['receiver']; ?></td>
+            <td class="py-2"><?php echo $rows['balance']; ?> </td>
+           
+                
+        <?php
+            }
+
+        ?>
+        </tbody>
+    </table>
+
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ userdetail.php
@@ -0,0 +1,233 @@
+<?php
+include 'connectdb.php';
+
+if(isset($_POST['submit']))
+{
+    $from = $_GET['ID'];
+    $to = $_POST['to'];
+    $amount = $_POST['amount'];
+
+    $sql = "SELECT * from customers where ID=$from";
+    $query = mysqli_query($conn,$sql);
+    $sql1 = mysqli_fetch_array($query); 
+
+    $sql = "SELECT * from customers where ID=$to";
+    $query = mysqli_query($conn,$sql);
+    $sql2 = mysqli_fetch_array($query);
+
+
+
+    
+    if (($amount)<0)
+   {
+        echo '<script>';
+        echo ' alert("Not a valid amount")'; 
+        echo '</script>';
+    }
+
+
+  
+    
+    else if($amount > $sql1['balance']) 
+    {
+        
+        echo '<script type="text/javascript">';
+        echo ' alert("Not enough balance")';  
+        echo '</script>';
+    }
+    
+
+
+   
+    else if($amount == 0){
+
+         echo "<script type='text/javascript'>";
+         echo "alert('Invalid Money Amount')";
+         echo "</script>";
+     }
+
+
+    else {
+        
+                // deducting amount from sender's account
+                $newbalance = $sql1['balance'] - $amount;
+                $sql = "UPDATE customers set balance=$newbalance where id=$from";
+                mysqli_query($conn,$sql);
+             
+
+                // adding amount to reciever's account
+                $newbalance = $sql2['balance'] + $amount;
+                $sql = "UPDATE customers set balance=$newbalance where id=$to";
+                mysqli_query($conn,$sql);
+                
+                $sender = $sql1['name'];
+                $receiver = $sql2['name'];
+                $sql = "INSERT INTO transaction(`sender`, `receiver`, `balance`) VALUES ('$sender','$receiver','$amount')";
+                $query=mysqli_query($conn,$sql);
+
+                if($query){
+                     echo "<script> alert('Transaction done');
+                                     window.location='history.php';
+                           </script>";
+                    
+                }
+
+                $newbalance= 0;
+                $amount =0;
+        }
+    
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/table.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>BANK OF SPAIN- Transaction</title>
+
+    <style>
+    	@import url('https://fonts.googleapis.com/css2?family=Luxurious+Roman&family=Oswald:wght@500&family=Permanent+Marker&display=swap');
+
+		
+        .navigation{
+    background-color: #ff0707!important;
+    
+        }
+        .navigation .navbar-brand{
+            font-size: 1.8rem;
+            padding-left: 40px;
+            font-family: 'Permanent Marker', cursive;
+            letter-spacing: 0.8px;
+            word-spacing: 1px;
+            
+            color: aliceblue!important;    
+        }
+        .navigation .navbar-brand:hover{
+                color:black!important;
+            }
+        .nav-link{
+            font-weight: bold;
+            font-size: 15px; 
+            color: antiquewhite!important; 
+        }
+        .btn{
+            background-color: #ff0707 !important;
+            color: white!important;
+        }
+        .btn:hover{
+            background-color: #078fff !important;
+            color: white!important;
+        }
+    </style>
+</head>
+<body>
+    
+    
+    <!-- navigation bar -->
+
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light py-1 navigation ">
+            <a class="navbar-brand" href="#">BANK OF SPAIN</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item ml-4">
+                  <a class="nav-link" href="index.html">HOME</a>
+                </li>
+                <li class="nav-item ml-4">
+                  <a class="nav-link" href="contact.html">CONTACT</a>
+                </li>
+                <li class="nav-item ml-4">
+                  <a class="nav-link" href="about.html">ABOUT</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+     </header>
+
+ 
+<br>
+
+	<div class="container">
+        <h2 class="text-center pt-2">Transaction</h2>
+            <?php
+                include 'connectdb.php';
+                $sid=$_GET['ID'];
+                $sql = "SELECT * FROM  customers where ID=$sid";
+                $result=mysqli_query($conn,$sql);
+                if(!$result)
+                {
+                    echo "Error : ".$sql."<br>".mysqli_error($conn);
+                }
+                $rows=mysqli_fetch_assoc($result);
+            ?>
+            <form method="post" name="tcredit" class="tabletext" ><br>
+        <div>
+            <table class="table table-striped table-condensed table-bordered">
+                <thead>
+                <tr>
+                    <th class="text-center">Id</th>
+                    <th class="text-center">Name</th>
+                    <th class="text-center">Email</th>
+                    <th class="text-center">Balance( In €)</th>
+                </tr>
+            </thead>
+                <tr>
+                    <td class="py-2"><?php echo $rows['ID'] ?></td>
+                    <td class="py-2"><?php echo $rows['name'] ?></td>
+                    <td class="py-2"><?php echo $rows['email'] ?></td>
+                    <td class="py-2"><?php echo $rows['balance'] ?></td>
+                </tr>
+            </table>
+        </div>
+        <br><br><br>
+        <label>Transfer To:</label>
+        <select name="to" class="form-control" required>
+            <option value="" disabled selected>Choose</option>
+            <?php
+                include 'connectdb.php';
+                $sid=$_GET['ID'];
+                $sql = "SELECT * FROM customers where ID!=$sid";
+                $result=mysqli_query($conn,$sql);
+                if(!$result)
+                {
+                    echo "Error ".$sql."<br>".mysqli_error($conn);
+                }
+                while($rows = mysqli_fetch_assoc($result)) {
+            ?>
+                <option class="table" value="<?php echo $rows['ID'];?>" >
+                
+                    <?php echo $rows['name'] ;?> 
+               
+                </option>
+            <?php 
+                } 
+            ?>
+            <div>
+        </select>
+        <br>
+        <br>
+            <label>Amount:</label>
+            <input type="number" class="form-control" name="amount" required>   
+            <br><br>
+                <div class="text-center" >
+            <button class="btn  mt-2" name="submit" type="submit" id="myBtn">Transfer</button>
+            </div>
+        </form>
+    </div>
+
+       
+        
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</body>
+</html>
