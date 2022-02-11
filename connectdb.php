4b32c6917baa7ceceeb80e7f3f8ef3b9f63d618f
Ankit Bhujeja <bhujejaankit@gmail.com>
2022-02-12 00:29:03 +0530

    Added

--- connectdb.php
+++ connectdb.php
@@ -4,8 +4,5 @@ try {
     $conn = new PDO("sqlsrv:server = tcp:basicbankwebbbbbb.database.windows.net,1433; Database = basicbankweb", "ankit", "Surat@123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    $conn->close();
-}
+
 
